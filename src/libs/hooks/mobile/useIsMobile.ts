2ef7578e914a640665e9a952aeec654e4dd3644a
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-10-10 20:16:02 +0300

    revert: returned Ania's implementation of useIsMobile hook to avoid concerns regarding its implementation

--- src/libs/hooks/mobile/useIsMobile.ts
+++ src/libs/hooks/mobile/useIsMobile.ts
@@ -1,21 +1,16 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
-export const useIsMobile = () => {
+export const useIsMobile = (breakpoint: number = 768) => {
   const [isMobile, setIsMobile] = useState(false);
 
-  const handleResize = useCallback(() => {
-    const isSmallScreen = window.innerWidth <= 768;
-    const isTouchDevice =
-      "ontouchstart" in window || navigator.maxTouchPoints > 0;
-    setIsMobile(isSmallScreen && isTouchDevice);
-  }, []);
-
   useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < breakpoint);
+    };
     handleResize();
     window.addEventListener("resize", handleResize);
-
     return () => window.removeEventListener("resize", handleResize);
-  }, [handleResize]);
+  }, [breakpoint]);
 
   return isMobile;
 };
