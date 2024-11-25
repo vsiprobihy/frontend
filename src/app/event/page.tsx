1e161ac569f8f6b826ce7e70c02f3d395ae878cf
PavelM <myagkiypavel@gmail.com>
2024-11-25 17:57:34 +0200

    fix: kebab case folder name

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { EventHeader } from "./_components/eventHeader/EventHeader";
-import { EventSummary } from "./_components/eventSummary/EventSummary";
-import { EventDetails } from "./_components/eventDetails/EventDetails";
+import { EventHeader } from "./_components/event-header/EventHeader";
+import { EventSummary } from "./_components/event-summary/EventSummary";
+import { EventDetails } from "./_components/event-details/EventDetails";
 
 const page = () => (
   <div className="m-auto mt-20 w-full max-w-[1174px] border-grey-light-middle p-[10px] ~mb-10/40">
